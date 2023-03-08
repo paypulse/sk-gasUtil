e486ea927a29aae83a75d74cbdcd1a1ac0a5bb34
paypulse <sourcedev5@gmail.com>
2023-03-08 18:28:42 +0900

    엑셀 다운로드 템플릿 추가

---
+++ src/main/java/com/example/skgasutils/excelDownload/ExcelDownload.java
@@ -0,0 +1,15 @@
+package com.example.skgasutils.excelDownload;
+
+
+import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@NoArgsConstructor
+@RestController
+public class ExcelDownload {
+
+
+
+}
