05963c5161f7138a3faeabda3b8af3c98faad9a4
paypulse <sourcedev5@gmail.com>
2023-03-31 17:11:10 +0900

    stream forEach 적용

--- src/main/java/com/example/skgasutils/EvuCdpUtil/Service/imple/EvuCdpUtilImple.java
+++ src/main/java/com/example/skgasutils/EvuCdpUtil/Service/imple/EvuCdpUtilImple.java
@@ -1,12 +1,21 @@
 package com.example.skgasutils.EvuCdpUtil.Service.imple;
 
 import com.example.skgasutils.EvuCdpUtil.Service.EvuCdpUtilService;
+import com.example.skgasutils.EvuCdpUtil.evucdpVo.EvuCdpVo;
+import com.example.skgasutils.Utils.CommonUtil;
 import com.example.skgasutils.mapper.CommonMapper;
 import com.example.skgasutils.mapper.EvuCdppUtilMapper;
+import com.example.skgasutils.repository.EvuCdp;
+import com.example.skgasutils.repository.EvuCdpComp;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import com.example.skgasutils.repository.EvuMng;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 
 @Slf4j
@@ -24,19 +33,41 @@ public class EvuCdpUtilImple implements EvuCdpUtilService {
 
     @Override
     public int insertEvuCdpCapa(String lastEvuStdId, String nowEvuStdId) {
-        //공통
-        System.out.println(commonMapper.getEvuCdpComp(lastEvuStdId));
-        List<EvuCdpComp> test = commonMapper.getEvuCdpComp(lastEvuStdId);
+
+        CommonUtil util = new CommonUtil();
+        String date = util.nowDate1("yyyy-MM-dd HH:mm:ss");
+        List<EvuCdpVo> insertCdp = new ArrayList<>();
+        List<EvuCdpVo> insertCdpComp = new ArrayList<>();
+
+        //cdp 직무
+        List<EvuCdp> cdp = commonMapper.getEvuCdp(lastEvuStdId);
+        //cdp안에서 바꿀 때
+        cdp.stream().forEach(n ->{
+            n.setEvuStdId("202301");
+            n.setInsYmdhms(date);
+            n.setInsUserId("00812");
+        });
+        
+
+
+
+        //cdp_comp cdp직무별 역량 평가 항목 관리
+        List<EvuCdpComp> cdpComp = commonMapper.getEvuCdpComp(lastEvuStdId);
+        //cdpComp 안에서 바꿀때
+        cdpComp.stream().forEach(n->{
+            n.setEvuCdpCompId("COM2023"+n.getEvuCdpCompId().substring(8,20));
+            n.setEvuStdId("202301");
+            n.setInsUserId("00812");
+            n.setInsYmdhms(date);
+        });
 
 
 
 
-        //evu_cdp insert
 
 
 
 
-        //evu_cdp_comp insert
 
 
         return 0;
--- src/main/java/com/example/skgasutils/Utils/CommonUtil.java
+++ src/main/java/com/example/skgasutils/Utils/CommonUtil.java
@@ -1,10 +1,21 @@
 package com.example.skgasutils.Utils;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class CommonUtil {
 
     /**
-     * evu_cdp_comp_id 만들어 주기
+     * 현재 날짜 주어진 양식에 맞게 출력
     **/
+    public String nowDate1(String format){
+        SimpleDateFormat sFormat = new SimpleDateFormat(format);
+
+        Date time = new Date();
+        String result = sFormat.format(time);
+        return result;
+    }
+
 
 
 }
